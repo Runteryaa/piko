6954703ef86df15e2fc19341ae83b179e4d02753
Runteryaa <Runteryaa@users.noreply.github.com>
2026-08-22 20:16:16 +0300

    fix(Instagram): fix startup crash in HideReshareButtonPatch by using safe returnBoxedBooleanEarly

--- patches/src/main/kotlin/app/crimera/patches/instagram/links/misc/HideReshareButtonPatch.kt
+++ patches/src/main/kotlin/app/crimera/patches/instagram/links/misc/HideReshareButtonPatch.kt
@@ -8,17 +8,14 @@ package app.crimera.patches.instagram.links.misc
 
 import app.crimera.patches.instagram.misc.settings.settingsPatch
 import app.crimera.patches.instagram.utils.Constants.COMPATIBILITY_INSTAGRAM
-import app.crimera.patches.instagram.utils.Constants.PREF_CALL_DESCRIPTOR
 import app.crimera.patches.instagram.utils.enableSettings
 import app.morphe.patcher.Fingerprint
 import app.morphe.patcher.extensions.InstructionExtensions.addInstructions
-import app.morphe.patcher.extensions.InstructionExtensions.addInstructionsWithLabels
-import app.morphe.patcher.extensions.InstructionExtensions.getInstruction
 import app.morphe.patcher.literal
 import app.morphe.patcher.patch.bytecodePatch
 import app.morphe.patcher.string
-import app.morphe.patcher.util.smali.ExternalLabel
 import app.morphe.patches.all.misc.resources.resourceMappingPatch
+import app.morphe.util.returnBoxedBooleanEarly
 
 // Credits: brosssh
 // https://github.com/brosssh/morphe-patches/commit/6a781ef8e0951ad5aa898fa17d094cfbfa5dd9fb
@@ -34,6 +31,7 @@ internal object FeedResponseMediaParserFingerprint : Fingerprint(
             literal(hashedFieldInteger),
         ),
     returnType = "Ljava/lang/Boolean;",
+    definingClass = "/LiveTreeMediaDict;",
 )
 
 internal object LiveTreeGetOptionalBooleanFingerprint : Fingerprint(
@@ -51,22 +49,7 @@ val hideReshareButtonPatch =
         compatibleWith(COMPATIBILITY_INSTAGRAM)
 
         execute {
-
-            val PREF_CALL = "$PREF_CALL_DESCRIPTOR->hideReshareButton()Z"
-
-            FeedResponseMediaParserFingerprint.method.apply {
-                addInstructionsWithLabels(
-                    0,
-                    """
-                    $PREF_CALL
-                    move-result v0
-                    if-eqz v0, :piko
-                    sget-object v0, Ljava/lang/Boolean;->FALSE:Ljava/lang/Boolean;
-                    return-object v0
-                    """.trimMargin(),
-                    ExternalLabel("piko", getInstruction(0)),
-                )
-            }
+            FeedResponseMediaParserFingerprint.method.returnBoxedBooleanEarly(false)
 
             // If it's trying to get the value for our field of interest via the Pando native library,
             // force the value to false instead
@@ -75,16 +58,14 @@ val hideReshareButtonPatch =
                 """
                 const v0, $hashedFieldInteger
                 if-ne p1, v0, :nopatch
-                $PREF_CALL
-                move-result v0
-                if-eqz v0, :nopatch
                 sget-object v0, Ljava/lang/Boolean;->FALSE:Ljava/lang/Boolean;
                 return-object v0
                 :nopatch
                 nop
-        """,
+                """.trimIndent(),
             )
 
             enableSettings("hideReshareButton")
         }
     }
+
